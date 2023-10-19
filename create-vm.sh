1583ac180fc9d5e8d623e72184cbc151af1493e0
James Williams <james@kananlabs.org>
2023-10-18 20:21:04 -0700

    Add local password.

--- create-vm.sh
+++ create-vm.sh
@@ -14,7 +14,7 @@ HOSTNAME="$2"
 # Create the ignition file
 tmp_ign_file=$(mktemp)
 trap 'rm -f "$tmp_ign_file"' EXIT
-envsubst '$HOSTNAME $TSKEY' < "$BUTANEFILE" | butane -ps -d contents -o "$tmp_ign_file"
+envsubst '$PASSWORD $HOSTNAME $TSKEY' < "$BUTANEFILE" | butane -ps -d contents -o "$tmp_ign_file"
 
 IGNITION_DEVICE_ARG=(--qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${tmp_ign_file}")
 
